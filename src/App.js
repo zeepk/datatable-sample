ad5f090882706da6aa4020319d4bc03100e0101c
zeepk <zeepkdb@gmail.com>
2020-09-01 21:57:45 -0700

    init datatable

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState, useEffect } from 'react';
+import { DataTable } from 'primereact/datatable';
+import { Column } from 'primereact/column';
+import 'bootswatch/dist/slate/bootstrap.min.css';
+// import 'primereact/resources/themes/nova-light/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [users, setUsers] = useState([]);
+	useEffect(() => {
+		fetch('https://jsonplaceholder.typicode.com/users')
+			.then((response) => response.json())
+			.then((data) => setUsers(data));
+	}, []);
+	return (
+		<div className="App">
+			<DataTable
+				style={{
+					borderRadius: '5px',
+					boxShadow: '1px 10px 29px 0px rgba(0,0,0,0.75)',
+					margin: '5vh 5vw',
+					padding: '20px',
+				}}
+				value={users}
+				rowHover
+			>
+				<Column field="username" header="Username" filter></Column>
+				<Column field="name" header="Name"></Column>
+			</DataTable>
+		</div>
+	);
 }
 
 export default App;
